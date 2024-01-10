444d2470b8780bdcfe24248b3eed320efd31b8ac
Pranjal Dhing <pranjaldhing1608@gmail.com>
2024-01-10 17:47:12 +0530

    authtoken created

---
+++ backend/routes/auth.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "ramchamakrahe"
+
+// create a user using POST "/api/auth/createuser". Doesn't require Auth
+router.post('/createuser',[
+    body('name', 'Enter a valid name').isLength({min : 3}),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({min : 5}),
+    
+], async(req,res)=>{
+    //If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({errors : errors.array() })
+    } 
+    //check whether the user with this email exists already
+
+    try{
+
+    let user = await User.findOne({email : req.body.email});
+    if(user) {
+        return res.status(400).json({error: "Sorry a user with this email already exists"})
+    }
+    const salt = await bcrypt.genSalt(10);
+    const secPass = await bcrypt.hash(req.body.password, salt)
+    //create a new user
+    user = await User.create({
+        name : req.body.name,
+        email : req.body.email,
+        password : secPass,
+    })
+    
+    // .then(user => res.json(user))
+    // .catch(err=> {console.log(err)
+    // res.json({error: 'Please enter a unique value for email', message: err.message})})
+    const data = {
+        user:{
+            id : user.id
+        }
+    }
+
+    const authtoken = jwt.sign(data, JWT_SECRET);
+    
+    res.json(authtoken)
+   }
+   catch(error) {
+    console.log(error.message);
+    res.status(500).send("some error occurred")
+   }
+
+})
+
+module.exports = router;
